4331c101bc4d0b6bae6a757d03a2e76f756ec236
fouledout443 <fouledout443@gmail.com>
2014-09-06 20:18:32 -0500

    bunch of missing shit....

---
+++ RentalApp/Models/Application.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using RentalApp.Models;
+
+//namespace RentalApp.Models
+//{
+//    public class Application
+//    {
+//        public int ApplicationID { get; set; }
+//        //foreign key
+//        public int UserID { get; set; }
+
+//        public virtual UserInfo U
+
+//        //public string FirstName { get; set; }
+//        //public string LastName { get; set; }
+//        //public string CurrentAddress { get; set; }
+//        //public string PreviousAddress { get; set; }
+//        //public string CurrentEmployer { get; set; }
+//        //public string PreviousEmployer { get; set; }
+
+
+
+
+
+//    }
+//}
---
+++ RentalApp/Models/Employer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalApp.Models
+{
+    public enum EmployerStatus {
+        Previous,
+        Current
+    }
+
+    public enum PayType {
+        Hourly,
+        Salary
+    }
+    public class Employer {
+        public int EmployerID { get; set; }
+        public string EmployerName { get; set; }
+        public string Location { get; set; }
+        public string ManagerName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string PositionTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string ReasonForLeaving { get; set; }
+        public string DurationOfEmployment { get; set; }
+        public PayType PayType { get; set; }
+    }
+}
