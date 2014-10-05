9bb7e37aa2762645a6b5fd057da036e5528de69b
JNolan <jakenolan80s@gmail.com>
2014-10-04 20:05:55 -0500

    trying a commit

---
+++ RentalApp/RentalApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalApp.Startup))]
+namespace RentalApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
